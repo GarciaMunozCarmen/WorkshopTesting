689b6dabaa9059a41654a44d96d46f05d24c7ae9
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-19 12:28:22 +0100

    aAdd CalculatorTest.java

---
+++ src/test/java/es/fplumara/dam1/worshoptesting/core/calculator/CalculatorTest.java
@@ -0,0 +1,9 @@
+package es.fplumara.dam1.worshoptesting.core.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CalculatorTest {
+
+}
