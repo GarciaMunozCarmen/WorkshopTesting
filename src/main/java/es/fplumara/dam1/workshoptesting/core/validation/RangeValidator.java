de4dc3de329d41d9cbd14cebf08bed8ed96410cc
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-22 13:42:49 +0100

    fix bug in ageValidator

--- src/main/java/es/fplumara/dam1/workshoptesting/core/validation/RangeValidator.java
+++ src/main/java/es/fplumara/dam1/workshoptesting/core/validation/RangeValidator.java
@@ -3,7 +3,7 @@ package es.fplumara.dam1.workshoptesting.core.validation;
 public class RangeValidator {
 
     public boolean isValidAge(int age) {
-        return age >= 0 && age <= 121;
+        return age >= 0 && age <= 120;
     }
 
     public boolean isNonBlank(String value) {
